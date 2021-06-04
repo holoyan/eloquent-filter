8a369af0c1a6cbb6b3502edf4f92f654706a2ce0
Artak <artaksafaryanc@gmail.com>
2021-06-04 20:45:54 +0400

    create test for predefined rules

--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -2,8 +2,6 @@
 
 namespace holoyan\EloquentFilter\Tests;
 
-use holoyan\EloquentFilter\Tests\User;
-
 class FilterTest extends BaseTestCase
 {
 
@@ -47,16 +45,71 @@ class FilterTest extends BaseTestCase
      */
     public function multiple_user_name_email_test()
     {
-        $user = User::create([
+        $user = User::inRandomOrder()->first();
 
+        User::create([
+            'name' => $user->name,
+            'email' => $user->email . 'test',
+            'b_date' =>$user->b_date,
+            'height' => $user->height
         ]);
+
         $request = [
-            'name' => $user->name . 'test',
-            'email' => $user->email,
+            'name' => $user->name,
+            'email' => $user->email . 'test',
         ];
 
         $filteredUser = User::filter($request, UserFilter::class)->get();
-        $this->assertEquals(0, $filteredUser->count());
+        $this->assertEquals(1, $filteredUser->count());
+
+        $filteredUser = User::filter(['name' => $user->name], UserFilter::class)->get();
+        $this->assertEquals(2, $filteredUser->count());
+    }
+
+
+    /**
+     * @test
+     */
+    public function raw_filter_test()
+    {
+        $user = User::inRandomOrder()->first();
+        $actualCount = User::where('height', '>', $user->height)->count();
+
+        $request = [
+            'height' => $user->height
+        ];
+
+        $filteredUser = User::filter($request, UserFilter::class)->get();
+        $this->assertEquals($actualCount, $filteredUser->count());
+    }
+
+    /**
+     * @test
+     */
+    public function relation_filter_rule()
+    {
+        $product = Product::inRandomOrder()->first();
+        // create products with same name for different users
+
+        foreach (User::inRandomOrder()->take(rand(2, 10))->get() as $user) {
+            Product::create([
+                'name' => $product->name,
+                'user_id' => $user->id
+            ]);
+        }
+
+        $actualCount = User::whereHas('products', function($query) use ($product){
+           $query->where('name', $product->name);
+        })->count();
+
+        $request = [
+            'products' => [
+                'name' => $product->name
+            ]
+        ];
+
+        $filteredUser = User::filter($request, UserFilter::class)->get();
+        $this->assertEquals($actualCount, $filteredUser->count());
     }
 
 }
--- tests/UserFilter.php
+++ tests/UserFilter.php
@@ -3,6 +3,8 @@
 namespace holoyan\EloquentFilter\Tests;
 
 use holoyan\EloquentFilter\Filter;
+use holoyan\EloquentFilter\Rules\RawRule;
+use holoyan\EloquentFilter\Rules\RelationRule;
 use holoyan\EloquentFilter\Rules\SimpleRule;
 
 class UserFilter extends Filter
@@ -12,7 +14,14 @@ class UserFilter extends Filter
     {
         return [
             'name' => SimpleRule::make(),
-            'email' => SimpleRule::make()->setComparisonType(SimpleRule::LIKE_COMPARISON_TYPES['both'])
+            'email' => SimpleRule::make()->setComparisonType(SimpleRule::LIKE_COMPARISON_TYPES['both']),
+            'height' => RawRule::make()->setCallback(function($builder, $column, $value){
+                $builder->where('height', '>', $value);
+            }),
+            'products' => RelationRule::make()->setRelation('products')->setRules([
+                'name' => SimpleRule::make(),
+                'user_id' => SimpleRule::make()
+            ])
         ];
     }
 }
